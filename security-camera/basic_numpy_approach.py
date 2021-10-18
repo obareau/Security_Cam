c65f78413acad796220a7cf703dda9407bc94f82
Olivier Bareau <olivier.bareau@gmail.com>
2021-10-18 22:24:59 +0200

    name change

--- security-camera/basic_numpy_approach.py
+++ security-camera/basic_numpy_approach.py
@@ -7,6 +7,9 @@ B = np.zeros((3, 3 ))
 s = 0
 for i in range(A.shape[0]):
     for j in range(A.shape[1]):
+        #  ! SSE
         s += (A[i,j] - B[i,j]) ** 2 # raise to ^2
 print(s)
-        
+# ! Numpy approach
+print(((A-B) **2 )) 
+print(((A-B) ** 2).sum())        
