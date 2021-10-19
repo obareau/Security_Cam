59e37de057063c5bd34aa4f24c52e29991e30a06
Olivier Bareau <olivier.bareau@gmail.com>
2021-10-19 05:48:06 +0200

    folder cleaning

--- main.py
+++ main.py
@@ -6,7 +6,7 @@
 import cv2
 
 # open webcam (import video)
-video_capture = cv2.VideoCapture('security-camera/video.mp4')
+video_capture = cv2.VideoCapture('video.mp4')
 # webcam : video_capture = cv2.VideoCapture(0)
 
 # read first frame
@@ -15,8 +15,18 @@ _, current_frame = video_capture.read()
 # convert to grayscale
 current_frame = cv2.cvtColor(current_frame, cv2.COLOR_BGR2GRAY)
 
-cv2.imshow ('app', current_frame)
-cv2.waitKey(0)
+# Main UI loop
+while True:
+    
+    # TODO : read in the next valid frame (and convert to grayscale)
+    _, current_frame = video_capture.read()
+    if current_frame is None:
+        break
+# TODO : compare two frame
+# TODO : display the video/wwebcam feed
+
+cv2.imshow('app', current_frame)
+cv2.waitKey(1)
 
 video_capture.release()
 cv2.destroyAllWindows
